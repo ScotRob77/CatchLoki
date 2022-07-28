7a164034799c98685fbad0ddcd58ede878e28f23
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-07-28 13:46:39 +0000

    Add functions for characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,63 @@
+let easyStart = document.getElementById('easy-game')
+easyStart.addEventListener('click', startEasy)
+// let tiles = document.getElementsByClassName('tile')
+var numTurnsSoFar = 0
+let score = 0
+var characterVisible = false
+var gameBoard = document.getElementById('game-board')
+//let characterClass = ""
 
-let startEasy = document.getElementById('easy-game')
-startEasy.addEventListener('click', easyGame)
 
-// Variables
-var loop = 0
-var tileVisible = true;
-var score = 0;
+function startEasy() {
+  easyGame();
+}
 
 
+// Easy game
 function easyGame() {
-    tileVisible = !tileVisible;
-    createHeroesVillans();
-    loop++;
-    if (loop < 20) {
-        setTimeout(easyGame, tileVisible ? 2000 : 3000);
-    } else {
-        alert("You scored " + score)
-        window.location.reload()
-    }
+  characterVisible = !characterVisible
+  newConfiguration()
+  flashCharacters()
+  numTurnsSoFar++
+  if (numTurnsSoFar < 10) {
+    setTimeout(easyGame, 3000)
+  } else {
+    alert('Game Over');
+  }
 }
 
-function hardGame() {
-    tileVisible = !tileVisible;
-    createHeroesVillans();
-    loop++;
-    if (loop < 20) {
-        setTimeout(hardGame, tileVisible ? 1000 : 2500);
-    } else {
-        alert("You scored " + score)
-        window.location.reload()
-    }
+// Creates the characters to flash on and off
+function flashCharacters() {
+  var characterClass = characterVisible ? 'character visible' : 'character hidden';
+  for (let i = 0; i < 6; i++) {
+    gameBoard.children[i].className = characterClass
+  }
 }
 
-function createHeroesVillans() {
-    var board = document.getElementById("game-board");
-    var classToSet = tileVisible ? "tile visible" : "tile hidden";
-    for (var i = 0; i < 6; i++) {
-        board.children[i].className = classToSet;
-        board.children[i].innerHTML = "";
-        board.children[i].onclick = function () {
-            score += -2;
-        }
-    }
-    var randomHero = Math.floor(Math.random() * 6) + 1;
-    board.children[randomHero - 1].innerHTML = "";
-    board.children[randomHero - 1].onclick = function () {
-        score += 10;
-    }
-    board.children[randomHero - 1].className = classToSet + " villain";
+function newConfiguration() {
+  for (let i = 0; i < 6; i++) {
+    gameBoard.children[i].innerHTML = "guest"
+  }
 }
+
+/*
+1. Game flow:
+
+a. When the user presses the start button
+
+   show new configuration
+
+b. When the user clicks on a character [tile] (when the game is active),
+
+   Update score (see 2)
+   show new configuration
+
+c. When we've reached the maximum number of turns, just stop the game
+
+2. Scoring
+a. If clicked on Loki, +1
+b. If clicked on Good guy, 0
+
+3. Stopping the game
+   Disable the tiles so they're not clickable
+*/
